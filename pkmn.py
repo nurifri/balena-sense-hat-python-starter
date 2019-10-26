7e2654f12370f4df93b7d6005526f99a95d75998
Arnau <arnaugamez@protonmail.com>
2019-10-26 13:18:43 +0200

    Fix pkmn

--- pkmn.py
+++ pkmn.py
@@ -1,5 +1,7 @@
-import imageio
-pkmn = imageio.imread('img/pkmn.png')
+from PIL import Image
+import numpy as np
+
+pkmn = np.array(Image.open('img/pkmn.png'))
 
 def getCharmander():
     return pkmn[0:8,48:56,:].reshape(64, 3).tolist()
